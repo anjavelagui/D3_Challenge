a1b8750aae3bdb9e5cae35778c9121542bf9c491
anjavelagui <anjavelagui@yahoo.com>
2021-08-26 23:36:30 -0400

    updating the app file with scale functions codes

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -3,24 +3,54 @@
 var svgWidth = 900;
 var svgHeight = 500;
 
-var margin = {top:20, right: 40, botton: 60, left:50};
+var margin = {top:20, right: 40, botton: 80, left:100};
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
 //Create a SVG wrapper to hold chart and set margins 
-var svg = d3
-   .select('#scatter')
-   .append('svg')
+var svg = d3.select('#scatter').append('svg')
    .attr('width', svgWidth)
    .attr('height', svgHeight)
-var chartGroup =svg.append('g').attr("transform", `translate(${margin.left}, ${margin.top})`);
-// Use d3 to import the data
-d3.csv("data/data.csv").then(function (data) {
-    data.forEach(function (data) {
-    data.poverty = +data.poverty;
-    data.healthcare = +data.healthcare;
-    data.obesity = +data.obesity;
-    data.age = +data.age;
-    data.income = +data.income;
-    data.smokes = +data.smokes;
+   .attr('class', "chart");
+
+//shift everything over by the margins
+var chartGroup = svg.append('g')
+   .attr("transform", `translate(${margin.left}, ${margin.top})`);
+d3.select("body").append("div").attr("class", "tooltip").style("opacity",0);
+
+// import data using D3 
+d3.csv("/data/data.csv", function(err, healthdata) {
+   if (err) throw err;
+   console.log(healthdata)
+   healthdata.forEach(function(data) {
+      data.poverty = +data.poverty; 
+      data.healthcare = +data.healthcare;
+      data.obesity = +data.obesity;
+      data.age = +data.age;
+      data.income = +data.income;
+      data.smokes = +data.smokes; 
+   });
+
+   //Scale functions
+   var xLinearScale = d3.scaleLinear().range([0, width]);
+   var yLinearScale = d3.scaleLinear().range([height, 0]);
+
+   //Create functions for axis
+   var bottomAxis = d3.axisBottom(xLinearScale);
+   var leftAxis = d3.axisLeft(yLinearScale);
+
+   var xMin;
+   var xMax;
+   var yMin;
+   var yMax;
+
+   xMin = d3.min(healthData, function (data) {
+      return data.healthcare;
+   });
+   
+
+
+
+    
+    
  });
